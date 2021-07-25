7faf4ec27b05160f3e9aec9891f8cca8ffff668a
Arslek <62049765+AlfViklund@users.noreply.github.com>
2021-07-25 21:31:20 +0500

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,38 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Override
+   public User userByCar(String model, int series) { return userDao.userByCar(model, series); }
+
+}
