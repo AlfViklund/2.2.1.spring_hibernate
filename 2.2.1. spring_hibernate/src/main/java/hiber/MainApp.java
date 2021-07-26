0ec23d58974f357f26e0550c0213f9ef18595680
Arslek <62049765+AlfViklund@users.noreply.github.com>
2021-07-26 12:53:02 +0500

    1 hql запрос с join-ом

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp {
       UserService userService = context.getBean(UserService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Jigul", 2)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Jigul", 5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Jigul", 7)));
       //userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       //userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
@@ -29,7 +29,7 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
-      long foundedUserId = userService.userByCar("Jigul", 5).getId();
+      long foundedUserId = userService.userByCar("Jigul", 7).getId();
       System.out.println("Id найденного юзера = " + foundedUserId);
 
       context.close();
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -31,14 +31,11 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public User userByCar(String model, int series) {
-      TypedQuery<Car> carQuery = sessionFactory.getCurrentSession().createQuery("from Car where model = :model and series =:series")
+      TypedQuery<User> query = sessionFactory.getCurrentSession()
+              .createQuery("from User u left join fetch u.car where u.car.model = :model and u.car.series =:series")
               .setParameter("model", model)
               .setParameter("series", series);
-      Long id = carQuery.getSingleResult().getId();
-      //User user = carQuery.getSingleResult().getUser();
-      TypedQuery<User> userQuery = sessionFactory.getCurrentSession().createQuery("from User where id =:car_id")
-              .setParameter("car_id", id);
-      return userQuery.getSingleResult();
+      return query.getSingleResult();
    }
 
 }
